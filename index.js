e2dac58faf0fe8fd5b06f1618da8a9d5bda81df6
João Vitor Fontoura <jvfontouraj@gmail.com>
2023-01-04 11:15:40 -0300

    update

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const fs = require("fs");
-const http = require("http");
-const { builtinModules } = require("module");
-const url = require("url");
-const slugify = require("slugify");
-const replaceTemplate = require("./modules/replaceTemplate");
+const fs = require('fs'); //require the File System Module, it allows to work with the file system on the pc
+const http = require('http'); //require the HTTP Module, allows Node.js to transfer data over HTTP
+const url = require('url'); //require the URL Module, splits up a web address into readable parts
+const slugify = require('slugify'); //require the Slugify Module, allows to customize the url
+const replaceTemplate = require('./modules/replaceTemplate'); //require the local module "replaceTemplate", that replace the templates from the HTML file and fill then with the information from "data.json" file
 
 //////////////////////////////
 ///FILES
@@ -36,62 +35,69 @@ const replaceTemplate = require("./modules/replaceTemplate");
 //////////////////////////////
 ///SERVER
 
+// "fs.readFileSync()" is a method to read a file and return its content
+// Syntax: fs.readFilseSync(path, options)
+//In this case, "options" is filled with the enconding
+
+//file content stored in a variable
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
-  "utf-8"
+  'utf-8'
 );
 const tempCard = fs.readFileSync(
   `${__dirname}/templates/template-card.html`,
-  "utf-8"
+  'utf-8'
 );
 const tempProduct = fs.readFileSync(
   `${__dirname}/templates/template-product.html`,
-  "utf-8"
+  'utf-8'
 );
 
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+//store the data from json file in a variable (DATA AS A STRING)
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+//the "JSON.parse()" method interprets the data and change it from a string to a js object
 const dataObj = JSON.parse(data);
 
+//
 const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
-console.log(slugs);
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
 
   //Overview page
-  if (pathname === "/" || pathname === "/overview") {
-    res.writeHead(200, { "Content-type": "text/html" });
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
 
     const cardsHtml = dataObj
       .map((el) => replaceTemplate(tempCard, el))
-      .join("");
-    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+      .join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
 
     res.end(output);
 
     //Product page
-  } else if (pathname === "/product") {
-    res.writeHead(200, { "Content-type": "text/html" });
+  } else if (pathname === '/product') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
     const product = dataObj[query.id];
     const output = replaceTemplate(tempProduct, product);
 
     res.end(output);
 
     //API
-  } else if (pathname === "/api") {
-    res.writeHead(200, { "content-type": "application/json" });
+  } else if (pathname === '/api') {
+    res.writeHead(200, { 'content-type': 'application/json' });
     res.end(data);
 
     //Not found
   } else {
     res.writeHead(404, {
-      "Content-type": "text/html",
-      "my-own-header": "hello-world",
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world',
     });
-    res.end("<h1>Page not found!</h1>");
+    res.end('<h1>Page not found!</h1>');
   }
 });
 
-server.listen(8000, "127.0.0.1", () => {
-  console.log("Listening to requests on port 8000");
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000');
 });
